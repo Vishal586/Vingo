08f4f0a8229937b1fcb9695de473088134a14885
Vishal Kumar <128839643+Vishal586@users.noreply.github.com>
2025-10-28 19:01:26 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -27,8 +27,8 @@ export const signUp=async (req,res) => {
 
         const token=await genToken(user._id)
         res.cookie("token",token,{
-            secure:false,
-            sameSite:"strict",
+            secure:true,
+            sameSite:"None",
             maxAge:7*24*60*60*1000,
             httpOnly:true
         })
@@ -55,8 +55,8 @@ export const signIn=async (req,res) => {
 
         const token=await genToken(user._id)
         res.cookie("token",token,{
-            secure:false,
-            sameSite:"strict",
+            secure:true,
+            sameSite:"None",
             maxAge:7*24*60*60*1000,
             httpOnly:true
         })
@@ -142,8 +142,8 @@ export const googleAuth=async (req,res) => {
 
         const token=await genToken(user._id)
         res.cookie("token",token,{
-            secure:false,
-            sameSite:"strict",
+            secure:true,
+            sameSite:"None",
             maxAge:7*24*60*60*1000,
             httpOnly:true
         })
@@ -154,4 +154,5 @@ export const googleAuth=async (req,res) => {
     } catch (error) {
          return res.status(500).json(`googleAuth error ${error}`)
     }
+
 }
